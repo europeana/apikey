749ab71c7182f5cca6db251495e54f674a140374
Patrick Ehlert <patrick.ehlert@europeana.eu>
2017-04-10 10:26:55 +0200

    Enable login via in-memory test user

--- oauth2-service/src/main/java/eu/europeana/oauth2/AuthorizationServer.java
+++ oauth2-service/src/main/java/eu/europeana/oauth2/AuthorizationServer.java
@@ -2,18 +2,69 @@ package eu.europeana.oauth2;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by Patrick Ehlert on 5-4-17.
  */
 @SpringBootApplication
 @EnableAuthorizationServer
-public class AuthorizationServer {
+@RestController
+public class AuthorizationServer extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(AuthorizationServer.class).properties("spring.config.name=authorizationServer").run(args);
     }
+
+    /**
+     * For now for testing the login we add 1 user
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("test").roles("USER");
+    }
+
+    /**
+     * Handle requests for (current) user information
+     * @param principal
+     * @return
+     */
+    @RequestMapping({ "/user", "/me" })
+    public Map<String, String> user(Principal principal) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", principal.getName());
+        return map;
+    }
+
+    /**
+     * Setup resource server to provide user details
+     */
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
+            // @formatter:on
+        }
+    }
+
 }
 
 
