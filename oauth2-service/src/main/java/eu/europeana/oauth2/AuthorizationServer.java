b0d40de6624bd2f9c3d8f95407c3af76d27395e6
Patrick Ehlert <patrick.ehlert@europeana.eu>
2017-04-20 11:31:29 +0200

    Refactor code and add JWT tokens
    The ResourceServer (and configuration) was moved to a separate class

--- oauth2-service/src/main/java/eu/europeana/oauth2/AuthorizationServer.java
+++ oauth2-service/src/main/java/eu/europeana/oauth2/AuthorizationServer.java
@@ -3,32 +3,33 @@ package eu.europeana.oauth2;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 /**
- * OAuth2 authorization and resource server.
- * To issue a token run on the command-line
- *    curl simpleTestClient:simpleSecret@localhost:8888/oauth/token -d grant_type=client_credentials
+ * OAuth2 authorization server.
+ *
+ * To issue a token run the following command:
+ *    curl --user "simpleTestClient:simpleSecret" http://localhost:8888/oauth/token -d grant_type=client_credentials -d scope=read
  *  or
- *    curl simpleTestClient:simpleSecret@localhost:8888/oauth/token -d grant_type=password -d username=user -d password=test
+ *    curl --user "simpleTestClient:simpleSecret" http://localhost:8888/oauth/token -d grant_type=password -d username=user -d password=test
+ *
+ * Running this later will also show you the existing token for that client or user again.
+ *
+ * After you obtained a token you can refresh it using
+ *    curl --user "simpleTestClient:simpleSecret" http://localhost:8888/oauth/token -d grant_type=refresh_token -d refresh_token=[insert_refresh_token]
+ *
  * Created by Patrick Ehlert on 5-4-17.
  */
 @SpringBootApplication
-@EnableAuthorizationServer
-@RestController
-// order defined to make sure any rules for "/user" and "/me" take precedence over the ones in the ResourceServer
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class AuthorizationServer extends WebSecurityConfigurerAdapter {
 
@@ -40,31 +41,47 @@ public class AuthorizationServer extends WebSecurityConfigurerAdapter {
         new SpringApplicationBuilder(AuthorizationServer.class).properties("spring.config.name=authorizationServer").run(args);
     }
 
-    /**
-     * Handle requests for (current) user information
-     * @param principal
-     * @return
-     */
-    @RequestMapping({ "/user", "/me" })
-    public Map<String, String> user(Principal principal) {
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("name", principal.getName());
-        return map;
-    }
-
-    /**
-     * Setup resource server to provide user details
-     */
     @Configuration
-    @EnableResourceServer
-    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-            http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
-            // @formatter:on
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        /**
+         * A JSON Web Tokens (JWT) contains all user information encrypted in the token
+         * @return a new default JWT access token converter
+         */
+        @Bean
+        public JwtAccessTokenConverter jwtAccessTokenConverter() {
+            return new JwtAccessTokenConverter();
         }
-    }
+
+        /**
+         * Enable the JWT tokens
+         * @param endpoints
+         * @throws Exception
+         */
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.accessTokenConverter(jwtAccessTokenConverter());
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("simpleTestClient")
+                    .secret("simpleSecret")
+                    .authorizedGrantTypes("authorization_code","client_credentials", "password", "refresh_token")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read", "write")
+                    .and()
+                    .withClient("unit_test")
+                    .secret("test")
+                    .authorizedGrantTypes("authorization_code","client_credentials", "password", "refresh_token")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read", "write");
+        }
+
+  }
+
 
 }
 
