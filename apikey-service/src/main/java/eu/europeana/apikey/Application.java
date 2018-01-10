4becfca48e1486eb5e2695885cc0465a3f8bb77d
Luthien-in-edhil <maike.dulk@europeana.eu>
2018-01-10 11:53:27 +0100

    EA-915 update email sent to clients

--- apikey-service/src/main/java/eu/europeana/apikey/Application.java
+++ apikey-service/src/main/java/eu/europeana/apikey/Application.java
@@ -232,25 +232,28 @@ class MailConfig extends WebMvcConfigurerAdapter {
     @Bean
     public SimpleMailMessage apikeyCreatedMail() {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setText("Dear %s %s,%n%nThank you for registering for the Europeana API.%n%n" +
-                "These are your Europeana API keys: %n%n==========%n" +
-                "API key      : %s %nPrivate key  : %s %n==========%n%n" +
+        message.setText("Dear %s %s,%n%nThank you for registering for the Europeana API." +
+                "%n" +
+                "These are your Europeana API keys: %n%n===========================%n" +
+                "API key: \t\t%s %nPrivate key: \t%s %n===========================%n%n" +
                 "Please keep a safe record of these keys." +
                 "%n%n" +
                 "The private key is used for specific API methods that require additional user authentication while " +
                 "the public key must be used by all others, see https://pro.europeana.eu/resources/apis/intro#access." +
-                "%n%n" +
+                "%n" +
                 "Please do not share these keys with third parties or expose it in user interfaces or in markup, as the " +
                 "API keys are confidential and are for use by the client or user only." +
                 "%n%n" +
                 "Our technical documentation for all APIs is available at https://pro.europeana.eu/resources/apis which " +
                 "includes an API console for testing and community developed libraries for a variety of programming languages." +
-                "%n%n" +
+                "%n" +
                 "Please join us in the Europeana API Forum (https://groups.google.com/forum/?pli=1#!forum/europeanaapi) " +
                 "to ask questions to us and other developers and to give us your feedback on our API. " +
-                "You can also contact us directly by mailing api@europeana.eu; we'd be especially grateful if you would " +
-                "let us know about your implementation so that we can feature it in our application gallery on " +
-                "Europeana Pro: https://pro.europeana.eu/resources/apps" +
+                "%n" +
+                "You can also contact us directly by mailing api@europeana.eu." +
+                "%n%n" +
+                "We would be especially grateful if you would let us know about your implementation so that we can " +
+                "feature it in our application gallery on Europeana Pro: https://pro.europeana.eu/resources/apps." +
                 "%n%n" +
                 "Best regards," +
                 "%n" +
