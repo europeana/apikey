db300a6b3279cb65ed85e265b53104780997c6f5
Luthien-in-edhil <maike.dulk@europeana.eu>
2018-03-26 16:11:46 +0200

    removed logback

--- apikey-service/src/main/java/eu/europeana/apikey/SwaggerConfig.java
+++ apikey-service/src/main/java/eu/europeana/apikey/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("eu.europeana.iiif"))
+                .apis(RequestHandlerSelectors.basePackage("eu.europeana.apikey"))
                 .paths(PathSelectors.any())
                 .build();
     }
