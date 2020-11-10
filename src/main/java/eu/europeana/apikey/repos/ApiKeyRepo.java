110a9cb9b5869d801e591004be983f419668ef90
Luthien-in-edhil <luthien@parendili.org>
2020-11-10 19:15:36 +0100

    further refactoring and cleanup

--- src/main/java/eu/europeana/apikey/repos/ApiKeyRepo.java
+++ src/main/java/eu/europeana/apikey/repos/ApiKeyRepo.java
@@ -1,6 +1,5 @@
 package eu.europeana.apikey.repos;
 
-import eu.europeana.apikey.controller.ApiKeyController;
 import eu.europeana.apikey.domain.ApiKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import static eu.europeana.apikey.config.ApikeyDefinitions.TO_MIGRATE_KEYCLOAKID;
 
 @Repository
 public interface ApiKeyRepo extends JpaRepository<ApiKey, String> {
@@ -17,6 +18,6 @@ public interface ApiKeyRepo extends JpaRepository<ApiKey, String> {
     List<ApiKey> findAll();
     List<ApiKey> findByEmailAndAppName(String email, String appName);
 
-    @Query("SELECT a FROM ApiKey a WHERE (a.keycloakId is null OR a.keycloakId = '" + ApiKeyController.TO_MIGRATE_KEYCLOAKID + "')")
+    @Query("SELECT a FROM ApiKey a WHERE (a.keycloakId is null OR a.keycloakId = '" + TO_MIGRATE_KEYCLOAKID + "')")
     List<ApiKey> findAllKeysToMigrate();
 }
